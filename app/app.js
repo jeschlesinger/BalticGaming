e316f0a3378b0dc29ccebb812b0df57156a08777
jeschlesinger <j.ericschlesinger@gmail.com>
2017-04-06 11:14:09 +0200

    update Thread

--- app/app.js
+++ app/app.js
@@ -1,65 +1,65 @@
 // Declare app level module which depends on views, and components
-var gamnamehelper="";
-var descriptionhelper="";
-var imglinkhelper="";
+var gamnamehelper = "";
+var descriptionhelper = "";
+var imglinkhelper = "";
+var currentGame;
 var app = angular
     .module('myApp', [
-  'ngRoute'
-  ])
+        'ngRoute'
+    ])
     .controller(
         'HomeCtrl',
-        function($scope, $location, $http) {
-            $scope.games = [{"name": "Path of Exile"},{"name": "data"}];
+        function ($scope, $location, $http) {
+            $scope.games = [{"name": "Path of Exile"}, {"name": "data"}];
             console.log("alert")
 
         }
-
-
     ).controller(
         'ThreadCtrl',
-        function($scope, $location, $http) {
-            $http.get('data/gamethreads.json').then(function(response){
-                $scope.games=response.data;
+        function ($scope, $location, $http) {
+            $http.get('data/gamethreads.json').then(function (response) {
+                $scope.games = response.data;
             });
-            $scope.editItem = function(game){
-                gamenamehelper=game.name;
-                descriptionhelper=game.description;
-                imglinkhelper=game.img;
+            $scope.editItem = function (game) {
+                currentGame=game;
             }
         }
-
-
     ).controller(
         'CreateThreadCtrl',
-        function($scope, $location, $http) {
-            $scope.myFunc = function() {
+        function ($scope, $location, $http) {
+            $scope.myFunc = function () {
                 console.log()
-                $http.get('http://localhost:3000/postThread/'+$scope.name).then(function(response){
+                $http.get('http://localhost:3000/postThread/' + $scope.name).then(function (response) {
                     console.log(response.data);
                 });
 
             }
         }
-
-
+    ).controller(
+        'UpdateThreadCtrl',
+        function ($scope, $location, $http) {
+            $scope.name=currentGame.name;
+            $scope.image=currentGame.img;
+            $scope.textareaid=currentGame.description;
+        }
     ).controller(
         'InnerThreadCtrl',
-        function($scope, $location, $http) {
-            $scope.gamename=gamenamehelper;
-            $scope.gamedescr=descriptionhelper;
-            $scope.gameimg=imglinkhelper;
+        function ($scope, $location, $http) {
+            $scope.gamename = currentGame.name;
+            $scope.gamedescr = currentGame.description;
+            $scope.gameimg = currentGame.img;
+
         }
-
-
     )
-    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
 
-  $routeProvider
-      .when('/bilder',{templateUrl:"/BalticGaming/app/games.html"})
-      .when('/impressum',{templateUrl:"/BalticGaming/app/impressum.html"})
-      .when('/createthread',{templateUrl:"/BalticGaming/app/createthread.html"})
-      .when('/thread',{templateUrl:"/BalticGaming/app/thread.html"});
-  }]);
+        $routeProvider
+            .when('/bilder', {templateUrl: "/BalticGaming/app/games.html"})
+            .when('/impressum', {templateUrl: "/BalticGaming/app/impressum.html"})
+            .when('/createthread', {templateUrl: "/BalticGaming/app/createthread.html"})
+            .when('/thread', {templateUrl: "/BalticGaming/app/thread.html"})
+            .when('/updatethread', {templateUrl: "/BalticGaming/app/updateThread.html"});
+    }]);
 
 
--- app/node.js
+++ app/node.js
@@ -6,30 +6,21 @@ var https        =    require('https');
 var http         =     require('http');
 var app          =    express();
 var fs = require('fs');
-var fileName = './data/gamethreads.json';
-var file = require(fileName);
+
 
 var httpServer   = http.createServer(app);
 var bodyParser = require('body-parser')
 
-
-// Add headers
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
 
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
     next();
 });
 
