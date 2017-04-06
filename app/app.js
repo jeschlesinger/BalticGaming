d6b4e7392a0d21418286d7051d462ba0ee69a5dd
jeschlesinger <j.ericschlesinger@gmail.com>
2017-04-06 20:55:12 +0200

    crud full

--- app/app.js
+++ app/app.js
@@ -21,26 +21,53 @@ var app = angular
                 $scope.games = response.data;
             });
             $scope.editItem = function (game) {
-                currentGame=game;
+                currentGame = game;
             }
         }
     ).controller(
         'CreateThreadCtrl',
         function ($scope, $location, $http) {
-            $scope.myFunc = function () {
-                console.log()
-                $http.get('http://localhost:3000/postThread/' + $scope.name).then(function (response) {
+            $scope.send = function () {
+
+                var temp = $scope.image;
+                temp = encodeURIComponent(temp);
+                console.log(temp);
+                var url = 'http://localhost:3434/postThread/' + $scope.name + "/" + $scope.textareaid + "/" + temp;
+                url = url.replace(/ /g, "%20");
+                $http.get(url).then(function (response) {
                     console.log(response.data);
                 });
 
             }
+
         }
     ).controller(
         'UpdateThreadCtrl',
         function ($scope, $location, $http) {
-            $scope.name=currentGame.name;
-            $scope.image=currentGame.img;
-            $scope.textareaid=currentGame.description;
+            $scope.name = currentGame.name;
+            $scope.image = currentGame.img;
+            $scope.textareaid = currentGame.description;
+
+            $scope.update = function () {
+                var temp = $scope.image;
+                temp = encodeURIComponent(temp);
+                var url = 'http://localhost:3434/updateThread/' + $scope.name + "/" + $scope.textareaid + "/" + temp;
+                url = url.replace(/ /g, "%20");
+                $http.get(url).then(function (response) {
+                    console.log(response.data);
+                });
+
+            }
+            $scope.delete = function () {
+                var temp = $scope.image;
+                temp = encodeURIComponent(temp);
+                var url = 'http://localhost:3434/deleteThread/' + $scope.name;
+                url = url.replace(/ /g, "%20");
+                $http.get(url).then(function (response) {
+                    console.log(response.data);
+                });
+
+            }
         }
     ).controller(
         'InnerThreadCtrl',
--- app/node.js
+++ app/node.js
@@ -1,14 +1,14 @@
 /**
  * Created by jeric on 05.04.2017.
  */
-var express      =    require("express");
-var https        =    require('https');
-var http         =     require('http');
-var app          =    express();
+var express = require("express");
+var https = require('https');
+var http = require('http');
+var app = express();
 var fs = require('fs');
 
 
-var httpServer   = http.createServer(app);
+var httpServer = http.createServer(app);
 var bodyParser = require('body-parser')
 
 app.use(function (req, res, next) {
@@ -29,17 +29,66 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-
-app.get("/postThread/:name", function (req, res) {
-    var obj = {name:req.params.name}
+app.get("/postThread/:name/:desc/:image", function (req, res) {
+    var temp = req.params.image;
+    //temp=decodeURIComponent(temp)
+    console.log(req.params.name);
+    console.log(temp);
+    console.log(req.params.image);
+    var obj = {name: req.params.name, img: temp, description: req.params.desc}
     var json = fs.readFileSync('./data/gamethreads.json');
     var file = JSON.parse(json);
-
     file.push(obj);
     var configJSON = JSON.stringify(file);
     fs.writeFileSync('./data/gamethreads.json', configJSON);
 
 
 });
+app.get("/updateThread/:name/:desc/:image", function (req, res) {
 
-httpServer.listen(3000);
+    //var obj = {name:req.params.name, img:temp , description:req.params.desc}
+    var json = fs.readFileSync('./data/gamethreads.json');
+
+    var file = JSON.parse(json);
+    console.log(file.length)
+    for (var i = 0; i < file.length; i++) {
+
+        if (file[i].name==req.params.name) {
+            file[i].img = req.params.image;
+            file[i].description = req.params.desc;
+
+            break;
+        }
+
+    }
+
+    var configJSON = JSON.stringify(file);
+    fs.writeFileSync('./data/gamethreads.json', configJSON);
+
+
+});
+app.get("/deleteThread/:name", function (req, res) {
+
+    //var obj = {name:req.params.name, img:temp , description:req.params.desc}
+    var json = fs.readFileSync('./data/gamethreads.json');
+
+    var file = JSON.parse(json);
+    console.log(file.length)
+    var temp;
+    for (var i = 0; i < file.length; i++) {
+
+        if (file[i].name==req.params.name) {
+            temp=i;
+            break;
+        }
+
+    }
+    file.splice(temp, 1)
+
+    var configJSON = JSON.stringify(file);
+    fs.writeFileSync('./data/gamethreads.json', configJSON);
+
+
+});
+console.log("Server running...")
+httpServer.listen(3434);
